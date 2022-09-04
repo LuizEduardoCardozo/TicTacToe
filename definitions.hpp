92d7057346bd4ea3f5e66cb7a3c03431f3698dc2
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2022-09-04 20:47:35 -0300

    chore: add a file for shared constants definitions

---
+++ definitions.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#define SCREEN_WIDTH 768
+#define SCREEN_HEIGHT 1136
+
+#define SPLASH_SCREEN_SHOW_TIME 0.5
+#define SPLASH_SCREEN_BACKGROUND_FILEPATH "resources/splash-background.png"
+
