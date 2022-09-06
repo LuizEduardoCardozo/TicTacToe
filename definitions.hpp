d51b2a7662392120005fc99dc1253caea4554c12
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2022-09-06 12:20:37 -0300

    chore: minor adjustments in some definitions

--- definitions.hpp
+++ definitions.hpp
@@ -2,9 +2,14 @@
 
 #define GAME_WINDOW_TITLE "Game"
 
-#define SCREEN_WIDTH 768
-#define SCREEN_HEIGHT 1136
+#define SCREEN_WIDTH  520
+#define SCREEN_HEIGHT 768
 
-#define SPLASH_SCREEN_SHOW_TIME 0.5
+#define SPLASH_SCREEN_SHOW_TIME           0.5
 #define SPLASH_SCREEN_BACKGROUND_FILEPATH "resources/splash-background.png"
+
+#define MAIN_MENU_TITLE_FILEPATH               "resources/game-title.png"
+#define MAIN_MENU_BACKGROND_FILEPATH           "resources/mainmenu-background.png"
+#define MAIN_MENU_PLAY_BUTTON_FILEPATH         "resources/play-button.png"
+#define MAIN_MENU_PLAY_BUTTON_OUTER_FILEPATH   "resources/play-button-outer.png"
 
