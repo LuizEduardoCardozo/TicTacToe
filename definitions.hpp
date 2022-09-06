5c967079cdcedbfc6eea7d2b020f3d7822e436f0
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2022-09-06 17:52:09 -0300

    chore: add some definitions about the game states

--- definitions.hpp
+++ definitions.hpp
@@ -13,3 +13,24 @@
 #define MAIN_MENU_PLAY_BUTTON_FILEPATH         "resources/play-button.png"
 #define MAIN_MENU_PLAY_BUTTON_OUTER_FILEPATH   "resources/play-button-outer.png"
 
+#define GAME_BACKGROUND_FILEPATH               "resources/mainmenu-background.png"
+
+#define PAUSE_BUTTON_FILEPATH "resources/pause-button.png"
+
+#define X_PLAYER_FILEPATH         "resources/x-player.png"
+#define O_PLAYER_FILEPATH         "resources/o-player.png"
+#define X_WINNING_PLAYER_FILEPATH "resources/x-winning-player.png"
+#define O_WINNING_PLAYER_FILEPATH "resources/o-winning-player.png"
+
+#define X_PLAYER     8
+#define O_PLAYER     0
+#define EMPTY_SPACE -1
+
+#define STATE_PLAYING       98
+#define STATE_PAUSE         97
+#define STATE_WON           96
+#define STATE_LOSE          95
+#define STATE_PLACE         94
+#define STATE_ENEMY_PLAYING 93
+#define STATE_DRAW          92
+
